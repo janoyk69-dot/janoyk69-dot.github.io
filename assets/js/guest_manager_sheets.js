a3f44b0cfe4cbaf199f42364d587371e32b0ebbc
Sergio Cárcamo <SERGIOCARCAMOCH@GMAIL.COM>
2026-01-16 17:53:16 -0300

    BD EXCEL

--- assets/js/guest_manager_sheets.js
+++ assets/js/guest_manager_sheets.js
@@ -1,6 +1,6 @@
 // guest_manager_sheets.js
 // Reemplaza la URL con la de tu Web App de Google Apps Script
-const SHEETS_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbzr_j0k85BmCyMqLS5Wu7MGEnkgdvhH_apkiyo63PwhkUC1v_OkInv25yZURd4zBusn/exec";
+const SHEETS_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbwpNoNoVw09OJYlOmqP8nPwkaTlC0UYyky9oUFUVaz7nWT6_P8U17X3qNeEXfaJxhFk/exec";
 
 function saveGuestSheets(name, status) {
     fetch(SHEETS_WEBAPP_URL, {
